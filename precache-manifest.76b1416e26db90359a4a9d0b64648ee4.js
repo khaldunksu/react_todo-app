120031cb2c69b0714d39853b69ff175798c15adf
ksukhaldun <kseniya.khaldun@gmail.com>
2019-12-16 11:50:51 +0300

    Updates

---
+++ precache-manifest.76b1416e26db90359a4a9d0b64648ee4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c120abb2c5ae67d08307d8758acd78c0",
+    "url": "/react_todo-app/index.html"
+  },
+  {
+    "revision": "9e6fa37c2249832c48ee",
+    "url": "/react_todo-app/static/css/main.2dc2e9f8.chunk.css"
+  },
+  {
+    "revision": "5a76bad6fc1e05f761a2",
+    "url": "/react_todo-app/static/js/2.583b85e5.chunk.js"
+  },
+  {
+    "revision": "9e6fa37c2249832c48ee",
+    "url": "/react_todo-app/static/js/main.1452b54f.chunk.js"
+  },
+  {
+    "revision": "51af3b3c9c5f8ccb1c53",
+    "url": "/react_todo-app/static/js/runtime~main.df364381.js"
+  }
+]);
